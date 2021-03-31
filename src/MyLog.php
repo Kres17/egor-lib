2d891879111e81f4d4afafbb7e608bf832e9c3ea
Егор Креславский <kreslavskiy01@mail.ru>
2021-03-31 14:03:51 +0300

    1.0

---
+++ src/MyLog.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Kreslavskiy;
+
+use core\LogAbstract;
+use core\LogInterface;
+
+class MyLog extends LogAbstract implements LogInterface
+{
+    public static function log(string $str): void
+    {
+        self::Instance()->_log($str);
+    }
+
+    public static function write(): void
+    {
+        LogAbstract::Instance()->_write();
+    }
+
+    public function _write()
+    {
+        foreach ($this->log as $value) {
+            echo $value;
+        }
+        $d = new \DateTime();
+        $file = "./Log/". $d->format('d-m-Y\TH_i_s_u').".log";
+        if (!is_dir('./Log/'))
+        {
+            mkdir("./Log/");
+        }
+        file_put_contents($file, $this->log);
+    }
+    public function _log($str)
+    {
+        $this->log[] = $str;
+    }
+}
